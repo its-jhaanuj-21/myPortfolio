259eb3377e494e373993961db90d9dd75308161c
Anuj Kr. Jha <96413803+its-jhaanuj-21@users.noreply.github.com>
2025-05-03 19:19:00 +0530

    new commit

---
+++ assets/js/service-worker.js
@@ -0,0 +1,67 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+    '/', // index.html
+    '/index.html',
+    '/assets/css/styles.css',
+    
+    // Favicons and Manifest
+    '/assets/favicon/apple-touch-icon.png',
+    '/assets/favicon/favicon-96x96.png',
+    '/assets/favicon/favicon.ico',
+    '/assets/favicon/favicon.svg',
+    '/assets/favicon/site.webmanifest',
+    '/assets/favicon/web-app-manifest-192x192.png',
+    '/assets/favicon/web-app-manifest-512x512.png',
+
+    // Images
+    '/assets/images/awardceremony.jpg',
+    '/assets/images/conference.jpg',
+    '/assets/images/demostration.jpg',
+    '/assets/images/groupphoto2023-2025.jpg',
+    '/assets/images/hackathon.jpg',
+    '/assets/images/logo.png',
+    '/assets/images/profile.png',
+    '/assets/images/project1.png',
+    '/assets/images/project2.png',
+    '/assets/images/project3.jpeg',
+    '/assets/images/social-sprite.png',
+    '/assets/images/workshop.jpg',
+
+    // JavaScript
+    '/assets/js/script.js'
+];
+
+// Install event
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Activate event
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
+});
