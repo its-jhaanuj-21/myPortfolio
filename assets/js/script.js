098f257cfe0bff4734aa2e9059b9042db8ecc0b4
Anuj Kr. Jha <96413803+its-jhaanuj-21@users.noreply.github.com>
2025-05-03 19:51:13 +0530

    enhanced service worker

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,7 +87,7 @@ document.addEventListener('DOMContentLoaded', function() {
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker
-        .register('/servive-worker.js')
+        .register('/assets/js/service-worker.js')
         .then(registration => {
           console.log('✅ Service Worker registered: ', registration.scope);
         })
